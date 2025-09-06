f9bd08a158eebb601f47ed09a5dccb04a2428b65
Preetham-HD <preethamhd.cs23@bmsce.ac.in>
2025-09-06 13:47:45 +0530

    setting db

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mysql = require("mysql2");
+
 const app = express();
 const PORT = 3000;
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "preet2005***",
+    database: "reShare"
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error("Database connection failed: " + err.stack);
+        return;
+    }
+    console.log("Connected to MySQL");
+});
 
 app.get('/', (req, res) => {
-    res.send('Hello, Express!');
+    res.send('Hello welcome to reShare');
 });
 
 app.listen(PORT, () => {
